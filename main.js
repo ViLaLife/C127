fed1a6eb92636c1806dd3db05bce2d1cf1e0da88
Aldrina's Pallet <93104220+ViLaLife@users.noreply.github.com>
2022-11-10 19:47:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+song="";
+
+function preload(){
+    loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas=createCanvas(600,600);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 600, 600);
+}
+
+function play(){
+    song.play()
+}
